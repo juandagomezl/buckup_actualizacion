c8476c4548d2c25e90ec3583bb7837bb92ad70a5
Juan David Gomez Lopez <72229962+juandagomezl@users.noreply.github.com>
2021-06-18 09:13:49 -0500

    Final

---
+++ actualizacion.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import time
+import smtplib
+from datetime import datetime
+import os
+import email.message
+from IPython.display import HTML
+
+def ultima_actualizacion(ruta):
+    try: 
+        estado = os.stat(ruta)
+        fecha = time.localtime(estado.st_mtime)
+        fecha = datetime(fecha[0], fecha[1], fecha[2], fecha[3], fecha[4])
+    except Exception as error:
+        print("Verifique la ruta del archivo\n")
+        print(f"EL error que se presento fue: {error}")
+    else:
+        return fecha
+
+def envio_correo(origin_address, password, destination_address, message):
+#
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(origin_address, password)
+        server.sendmail(origin_address, destination_address, message)
+        server.quit()
+
+        
+def envio_correo_df (subject, origin_address, password, destination_address, message, df):       
+    
+    content_table = df.to_html()
+    msg = email.message.Message()
+    msg['Subject'] = subject
+
+
+    msg['From'] = origin_address
+    msg['To'] = destination_address # puede ser uno solo o una lista con varios correos
+    password = password
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(message + content_table)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+
+    s.sendmail(msg['From'], [msg['To']], msg.as_string())
+
+archivo_csv = 'archivos.csv'
+archivos = pd.read_csv(archivo_csv)
+archivos_off = pd.DataFrame()
+fecha_actual = datetime.now()
+
+for index, x in archivos.iterrows():
+    ruta = f"{x['ruta']}\\{x['archivo']}"
+    fecha_actualizizacion = ultima_actualizacion(ruta)
+
+    if (fecha_actual - fecha_actualizizacion).days > 0:
+       
+        archivos_off = archivos_off.append({'cliente': x['cod_cliente'], 
+                                            'tipo_proceso': x['tipo_proceso'],'nombre_archivo': x['archivo'], 'ultima_modificacion': fecha_actualizizacion}, ignore_index = True, sort = True)
+
+origin_address = 'juandagomezl12@gmail.com'
+password = 'judagolo970612'
+destination_address = 'judgomezlo@unal.edu.co' # tambien puede ser una lista con varios correos
+subject = 'Ultima Prueba'
+fecha_actual = datetime.strftime(fecha_actual,'%b %d, %Y')
+if len(archivos_off) == 0:
+    message = f"El dia {fecha_actual}, todos los archivos que se encuentra en {archivo_csv} fueron actualizados correctamente"
+else:
+    message = f"Los archivos que no se actualizaron correctamente hoy {fecha_actual}, segun la lista que se encuentra en {archivo_csv} fueron : \n\n\n\n"
+
+envio_correo_df (subject, origin_address, password, destination_address, message, archivos_off)
---
+++ backup_s3.py
@@ -0,0 +1,68 @@
+import shutil
+import boto3
+import pandas as pd
+from datetime import datetime
+import os
+
+
+
+def compress (new_path, extension, path):
+    
+    shutil.get_archive_formats()
+    shutil.make_archive(new_path, extension, path)
+
+def connection_s3(region, acces_key, secret_access_key):
+    
+    s3 = boto3.client('s3',
+        region_name = region,
+        aws_access_key_id = acces_key,
+        aws_secret_access_key = secret_access_key
+    )
+    return s3
+
+def backup_in_s3 (connection, path, bucket_name, file_name):
+    
+    connection.upload_file(
+        Filename = path,
+        Bucket= bucket_name,
+        Key= file_name
+    )  
+
+
+def backup (connection, path_csv):
+    print(1)
+    now = datetime.now().strftime('%Y%m%d')
+    paths = pd.read_csv(path_csv)
+    
+
+    for index, files in paths.iterrows():
+        file = f"{files['path_file']}\\{files['name_file']}"
+        fecha_actual = f"{now}\\{files['name_file']}"
+        print(2)
+        print(file)
+        print(fecha_actual)
+        shutil.copytree(file, fecha_actual)
+        fecha_actual = now
+        
+        
+    compress (f"{now}", "zip", f"{now}")
+    backup_in_s3 (connection, f"{now}.zip", "coosalud-direktio", f"backups/{now}.zip")
+    
+    os.remove(f"{now}.zip")
+    shutil.rmtree(f"{now}")
+    
+path_csv = 'prueba_buckup.csv'
+s3 = connection_s3('us-east-1', 'AKIAZXACQZEWB5VG244Z', 'Gh3CozSTHZLa/E/YDwHJ5kY+gYtdokgK5dPSDX1S')
+
+
+try: 
+
+    backup(s3, path_csv)  
+
+except FileNotFoundError as error:
+    mensaje = "El path o el file_name no se encontraron o se encuentran mal especificados"
+    print(f"Error -> {error}. {mensaje}")
+
+else:
+
+    print("buckup exitoso")
